5b598509ab0096b487526aedbb3fbfe7e177b66b
Aubrey Jones <aubrey_jones07@hotmail.com>
2021-03-02 00:23:33 -0700

    getQuestions function generates questions

--- src/App.svelte
+++ src/App.svelte
@@ -21,9 +21,7 @@ onMount(() => {
 		console.log(data)
 		categories = data.results[0].category; 
 		difficulties = data.results[0].difficulty; 
-		questions = data.results[0].question; 
-		correctAnswer = data.results[0].correct_answer; 
-		incorrectAnswers = data.results[0].incorrect_answers; 
+		
 	})
 })
 
@@ -31,8 +29,15 @@ onMount(() => {
 	async function getQuestions() {
 		const response = await fetch(`https://opentdb.com/api.php?amount=10&category=${selectedCat}&difficulty=${selectedDiff}`)
 		const data = await response.json(); 
+		
+		.then((data) => {
+		questions = data.results[0].question; 
+		correctAnswer = data.results[0].correct_answer; 
+		incorrectAnswers = data.results[0].incorrect_answers; 
 
 
+		})
+	
 	}
 	
 
@@ -40,13 +45,13 @@ onMount(() => {
 
 	function rightAnswer() {
 		count += 1;
-		console.log(count)
+		console.log(count++)
 	}
 	
 	
 	function wrongAnswer() {
 		count -= 1;
-		console.log(count)
+		console.log(count--)
 	}
 	
 	
@@ -62,21 +67,15 @@ onMount(() => {
 
 <label>Choose Category</label>
 <select bind:value="{selectedCat}" name="category" id="category">
-	<option value="">Please choose an option</option>
-	{#each categories as category}
-	<option value="{category.id}">{category.name}</option>
-	{/each}
+	<option disabled selected>Please choose an option</option>
+	<option>{categories}</option>
 	</select>
 </div>
 <div>
 <label>Choose Difficulty</label>
 <select bind:value="{selectedDiff}" name="difficulty" id="difficulty">
 	  <option disabled selected>Choose Difficulty</option>
-	{#each difficulties as difficulty}
-    <option value="1">{difficulty.easy}</option>
-    <option value="2">{difficulty.medium}</option>
-    <option value="3">{difficulty.hard}</option>
-	{/each}
+    <option>{difficulties}</option>
 	</select>
 </div>
 
@@ -87,10 +86,10 @@ onMount(() => {
 
 <div>
 <div>{questions}</div>
-<button>{correctAnswer}</button>
-<button>{incorrectAnswers[0]}</button>
-<button>{incorrectAnswers[1]}</button>
-<button>{incorrectAnswers[2]}</button>
+<button on:click={() => rightAnswer()}>{correctAnswer}</button>
+<button on:click={() => wrongAnswer()}>{incorrectAnswers[0]}</button>
+<button on:click={() => wrongAnswer()}>{incorrectAnswers[1]}</button>
+<button on:click={() => wrongAnswer()}>{incorrectAnswers[2]}</button>
 </div>
 
 
